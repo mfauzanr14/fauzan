ffd945be5793175563d2e72032e8518932759fdd
mfauzanr14 <85923848+mfauzanr14@users.noreply.github.com>
2021-06-15 19:14:09 +0800

    Add files via upload

---
+++ prosesedit.php
@@ -0,0 +1,17 @@
+<?php 
+// koneksi database
+include 'koneksi.php';
+ 
+// menangkap data yang di kirim dari form
+$id = $_POST['id'];
+$nama = $_POST['nama'];
+$alamat = $_POST['alamat'];
+$pekerjaan = $_POST['pekerjaan'];
+ 
+// update data ke database
+mysqli_query($db,"update user set nama='$nama', alamat='$alamat', pekerjaan='$pekerjaan' where id='$id'");
+ 
+// mengalihkan halaman kembali ke logbook.php
+header("location:index.php");
+ 
+?>
